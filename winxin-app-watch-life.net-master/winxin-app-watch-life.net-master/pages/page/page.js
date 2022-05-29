2b3da6faedc0bfce8b06b59cc4f776988cd01999
YM-huang <huangyimiao666@gmail.com>
2022-05-29 12:02:04 +0800

    droneapp

---
+++ winxin-app-watch-life.net-master/winxin-app-watch-life.net-master/pages/page/page.js
@@ -0,0 +1,92 @@
+/*
+ * 
+ * 微慕小程序开源版
+ * author: jianbo
+ * organization: 微慕  www.minapper.com
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * 开源协议：MIT
+ * Copyright (c) 2017  微慕 https://www.minapper.com All rights reserved.
+ * 
+ */
+
+import config from '../../utils/config.js'
+var Api = require('../../utils/api.js');
+var util = require('../../utils/util.js');
+var webSiteName= config.getWebsiteName;
+var domain =config.getDomain
+
+Page({
+  data: {
+    title: '页面内容',
+    pageData: {},
+    pagesList: {},
+    hidden: false,
+  
+    webSiteName:webSiteName,
+    domain:domain
+  },
+  onLoad: function (options) {
+    this.fetchData(options.id);
+    wx.showShareMenu({
+      withShareTicket:true,
+      menus:['shareAppMessage','shareTimeline'],
+      success:function(e)
+      {
+        //console.log(e);
+      }
+    })
+     
+  },
+  onShareTimeline: function() {
+    let imageUrl = this.data.pageData.post_full_image
+    if(imageUrl=='')
+    {
+      imageUrl=this.data.pageData.postImageUrl
+    }
+    return {
+      title: this.data.pageData.title.rendered,
+      query: {
+        id: this.data.pageData.id
+      },
+      imageUrl
+    }
+  },
+  
+  fetchData: function (id) {
+    var self = this;
+    self.setData({
+      hidden: false
+    });
+    wx.request({
+      url: Api.getPageByID(id, { mdrender: false }),
+      success: function (response) {
+        console.log(response);
+        self.setData({
+          pageData: response.data,         
+        });
+        setTimeout(function () {
+          self.setData({
+            hidden: true
+          });
+        }, 300);
+      }
+    });
+  },
+  fetchPagesData: function () {
+    var self = this;
+    wx.request({
+      url: Api.getPages(),
+      success: function (response) {
+        self.setData({
+          pagesList: response.data
+        });
+        setTimeout(function () {
+          self.setData({
+            hidden: true
+          });
+        }, 300);
+      }
+    });
+  }
+})
